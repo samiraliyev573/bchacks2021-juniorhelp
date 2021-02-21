23fb5cdf8e0f3cf9f3205acfe41fd00e82be2489
samiraliyev573 <aliyev7137@gmail.com>
2021-02-21 09:01:01 -0800

    Login/Registration Screen

--- juniorhelp/lib/screens/login.dart
+++ juniorhelp/lib/screens/login.dart
@@ -1,118 +1,174 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:juniorhelp/main.dart';
 import 'package:juniorhelp/screens/signup.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  bool showSpinner = false;
+  final _auth = FirebaseAuth.instance;
+  String email;
+  String password;
   final _emailFieldController = TextEditingController();
+
   final _passwordFieldController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       // backgroundColor: Gradient(
       //   colors: [Color(0xff2ec4b6,Color(0xff495057)]
       // ),
-      body: Container(
-        color: Colors.blueGrey,
-        child: Center(
-          child: Column(
-            children: [
-              SizedBox(height: 50),
-              Image(
-                image: AssetImage("assets/logo.png"),
-                width: 225,
-              ),
-              SizedBox(height: 20),
-              Text("Log In",
-                  style: TextStyle(fontSize: 40, color: Colors.white)),
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: TextField(
-                  cursorColor: Colors.white,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: SizedBox.expand(
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/background.png"),
+                  fit: BoxFit.cover),
+            ),
+            child: Center(
+              child: Column(
+                children: [
+                  SizedBox(height: 50),
+                  Image(
+                    image: AssetImage("assets/logo.png"),
+                    width: 225,
                   ),
-                  controller: _emailFieldController,
-                  decoration: InputDecoration(
-                      prefixIcon: Icon(
-                        Icons.email,
-                        color: Colors.white,
-                        size: 30,
+                  SizedBox(height: 20),
+                  Text("Log In",
+                      style: TextStyle(fontSize: 40, color: Colors.black)),
+                  Padding(
+                    padding: EdgeInsets.all(20),
+                    child: TextField(
+                      onChanged: (value) {
+                        email = value;
+                      },
+                      cursorColor: Colors.black,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
                       ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white, width: 1.0),
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                      ),
-                      border: OutlineInputBorder(),
-                      hintText: "Email",
-                      hintStyle: TextStyle(fontSize: 20.0, color: Colors.white),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white, width: 1.0),
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                      )),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: TextField(
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  obscureText: true,
-                  cursorColor: Colors.white,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                  controller: _passwordFieldController,
-                  decoration: InputDecoration(
-                      prefixIcon: Icon(
-                        Icons.lock,
-                        color: Colors.white,
-                        size: 30,
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white, width: 1.0),
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                      ),
-                      border: OutlineInputBorder(),
-                      hintText: "Password",
-                      hintStyle: TextStyle(fontSize: 20.0, color: Colors.white),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white, width: 1.0),
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                      )),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: FlatButton(
-                  onPressed: () {
-                    print("aa");
-                  },
-                  color: Colors.transparent,
-                  child: Container(
-                    decoration:
-                        BoxDecoration(border: Border.all(color: Colors.white)),
-                    padding: EdgeInsets.all(15.0),
-                    child: Center(
-                      child: Text('Sign In',
-                          style: TextStyle(fontSize: 25, color: Colors.white)),
+                      controller: _emailFieldController,
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.email,
+                            color: Colors.black,
+                            size: 30,
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.black, width: 1.0),
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                          ),
+                          border: OutlineInputBorder(),
+                          hintText: "Email",
+                          hintStyle:
+                              TextStyle(fontSize: 20.0, color: Colors.black),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.black, width: 1.0),
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                          )),
                     ),
                   ),
-                ),
+                  Padding(
+                    padding: EdgeInsets.all(20),
+                    child: TextField(
+                      onChanged: (value) {
+                        password = value;
+                      },
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      obscureText: true,
+                      cursorColor: Colors.black,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                      ),
+                      controller: _passwordFieldController,
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.lock,
+                            color: Colors.black,
+                            size: 30,
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.black, width: 1.0),
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                          ),
+                          border: OutlineInputBorder(),
+                          hintText: "Password",
+                          hintStyle:
+                              TextStyle(fontSize: 20.0, color: Colors.black),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.black, width: 1.0),
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                          )),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(20),
+                    child: FlatButton(
+                      onPressed: () async {
+                        setState(() {
+                          showSpinner = true;
+                        });
+                        try {
+                          final user = await _auth.signInWithEmailAndPassword(
+                              email: email, password: password);
+                          if (user != null) {
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) {
+                              return BottomNav();
+                            }));
+                          }
+
+                          setState(() {
+                            showSpinner = false;
+                          });
+                        } catch (e) {
+                          print(e);
+                        }
+                      },
+                      color: Colors.transparent,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black),
+                            color: Colors.black38),
+                        padding: EdgeInsets.all(15.0),
+                        child: Center(
+                          child: Text('Sign In',
+                              style:
+                                  TextStyle(fontSize: 25, color: Colors.black)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return SignUp();
+                      }));
+                    },
+                    child: Text(
+                      "Don't have an account? Sign Up",
+                      style: TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                  )
+                ],
               ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return SignUp();
-                  }));
-                },
-                child: Text(
-                  "Don't have an account? Sign Up",
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                ),
-              )
-            ],
+            ),
           ),
         ),
       ),
--- juniorhelp/lib/screens/signup.dart
+++ juniorhelp/lib/screens/signup.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:juniorhelp/main.dart';
 import 'package:juniorhelp/screens/home.dart';
 import 'package:juniorhelp/screens/login.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class SignUp extends StatefulWidget {
   @override
@@ -11,120 +13,160 @@ class SignUp extends StatefulWidget {
 class _SignUpState extends State<SignUp> {
   final _emailFieldController = TextEditingController();
   final _passwordFieldController = TextEditingController();
+  final _auth = FirebaseAuth.instance;
+  bool showSpinner = false;
+  String email;
+  String password;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       // backgroundColor: Gradient(
       //   colors: [Color(0xff2ec4b6,Color(0xff495057)]
       // ),
-      body: Container(
-        color: Colors.blueGrey,
-        child: Center(
-          child: Column(
-            children: [
-              SizedBox(height: 50),
-              CircleAvatar(
-                backgroundColor: Colors.white,
-                radius: 100,
-                child: Image(
-                  image: AssetImage("assets/logo.png"),
-                  width: 200,
-                ),
-              ),
-              SizedBox(height: 20),
-              Text("Sign Up",
-                  style: TextStyle(fontSize: 40, color: Colors.white)),
-              Padding(
-                padding: EdgeInsets.all(15),
-                child: TextField(
-                  cursorColor: Colors.white,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 15,
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: SizedBox.expand(
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/background.png"),
+                  fit: BoxFit.cover),
+            ),
+            child: Center(
+              child: Column(
+                children: [
+                  SizedBox(height: 50),
+                  Image(
+                    image: AssetImage("assets/logo.png"),
+                    width: 200,
                   ),
-                  controller: _emailFieldController,
-                  decoration: InputDecoration(
-                      prefixIcon: Icon(
-                        Icons.email,
-                        color: Colors.white,
-                        size: 30,
+                  SizedBox(height: 20),
+                  Text("Sign Up",
+                      style: TextStyle(fontSize: 40, color: Colors.black)),
+                  Padding(
+                    padding: EdgeInsets.all(15),
+                    child: TextField(
+                      onChanged: (value) {
+                        email = value;
+                      },
+                      cursorColor: Colors.black,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15,
                       ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white, width: 1.0),
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                      ),
-                      border: OutlineInputBorder(),
-                      hintText: "Email",
-                      hintStyle: TextStyle(fontSize: 20.0, color: Colors.white),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white, width: 1.0),
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                      )),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: TextField(
-                  obscureText: true,
-                  cursorColor: Colors.white,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                  controller: _passwordFieldController,
-                  decoration: InputDecoration(
-                      prefixIcon: Icon(
-                        Icons.lock,
-                        color: Colors.white,
-                        size: 30,
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white, width: 1.0),
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                      ),
-                      border: OutlineInputBorder(),
-                      hintText: "Password",
-                      hintStyle: TextStyle(fontSize: 20.0, color: Colors.white),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white, width: 1.0),
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                      )),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: FlatButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return BottomNav();
-                    }));
-                  },
-                  color: Colors.transparent,
-                  child: Container(
-                    decoration:
-                        BoxDecoration(border: Border.all(color: Colors.white)),
-                    padding: EdgeInsets.all(15.0),
-                    child: Center(
-                      child: Text('Sign Up',
-                          style: TextStyle(fontSize: 25, color: Colors.white)),
+                      controller: _emailFieldController,
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.email,
+                            color: Colors.black,
+                            size: 30,
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.black, width: 1.0),
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                          ),
+                          border: OutlineInputBorder(),
+                          hintText: "Email",
+                          hintStyle:
+                              TextStyle(fontSize: 20.0, color: Colors.black),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.black, width: 1.0),
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                          )),
                     ),
                   ),
-                ),
+                  Padding(
+                    padding: EdgeInsets.all(20),
+                    child: TextField(
+                      onChanged: (value) {
+                        password = value;
+                      },
+                      obscureText: true,
+                      cursorColor: Colors.black,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                      ),
+                      controller: _passwordFieldController,
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.lock,
+                            color: Colors.black,
+                            size: 30,
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.black, width: 1.0),
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                          ),
+                          border: OutlineInputBorder(),
+                          hintText: "Password",
+                          hintStyle:
+                              TextStyle(fontSize: 20.0, color: Colors.black),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.black, width: 1.0),
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                          )),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(20),
+                    child: FlatButton(
+                      onPressed: () async {
+                        setState(() {
+                          showSpinner = true;
+                        });
+                        try {
+                          final newUser =
+                              await _auth.createUserWithEmailAndPassword(
+                                  email: email, password: password);
+                          if (newUser != null) {
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) {
+                              return BottomNav();
+                            }));
+                          }
+
+                          setState(() {
+                            showSpinner = false;
+                          });
+                        } catch (e) {
+                          print(e);
+                        }
+                      },
+                      color: Colors.black38,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black)),
+                        padding: EdgeInsets.all(15.0),
+                        child: Center(
+                          child: Text('Sign Up',
+                              style:
+                                  TextStyle(fontSize: 25, color: Colors.black)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return Login();
+                      }));
+                    },
+                    child: Text(
+                      "Take me back to Login",
+                      style: TextStyle(fontSize: 20, color: Colors.black),
+                    ),
+                  )
+                ],
               ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return Login();
-                  }));
-                },
-                child: Text(
-                  "Already have an account? Sign In",
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                ),
-              )
-            ],
+            ),
           ),
         ),
       ),
